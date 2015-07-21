88eb2032a50255eb5b129413f692e710a54e88cb
brian m. carlson <sandals@crustytoothpaste.net>
2015-07-21 02:32:41 +0000

    Add a spec for basic UI checking.
    Check that there's a main page, that it's well-formed XHTML, and that
    it contains at least some useful content.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ spec/ui_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../app/run'
+
+describe 'the UI' do
+  def app
+    Sinatra::Application
+  end
+
+  it 'has a main page' do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to be_well_formed
+  end
+
+  it 'lists comics by name on the main page' do
+    get '/'
+    expect(last_response.body).to match(/<li>Dilbert.*<\/li>/)
+  end
+end
